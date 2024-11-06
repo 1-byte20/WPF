6ce16fa966791bb0517c87b283f88708a44b2973
Bite Gergő Máté <153619640+1-byte20@users.noreply.github.com>
2024-11-06 15:06:11 +0100

    Add files via upload

---
+++ tavaly/virágkötés/virágkötés/Model/Virágbolt.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virágkötés.Model
+{
+    public class Virágbolt
+    {
+        public int tulipan;
+        public int rosza;
+        public int narcisz;
+        public int osszeg;
+
+        public Virágbolt()
+        {
+            tulipan = 1;
+            rosza = 1;
+            narcisz = 1;
+        }
+
+        public int Tulipandb
+        {
+            get { return tulipan; }
+            set { tulipan = value; }
+        }
+
+        public int Roszadb
+        {
+            get { return rosza; }
+            set { rosza = value; }
+        }
+
+        public int Narciszdb
+        {
+            get { return narcisz; }
+            set { narcisz = value; }
+        }
+
+        public int Vegosszeg
+        {
+            get 
+            {
+                int osszeg = (300 * tulipan) + (500 * rosza) + (400 * narcisz);
+                return osszeg;
+            }
+        }
+    }
+}
---
+++ tavaly/virágkötés/virágkötés/WiewModel/ViragWiewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModels.BaseClass;
+using virágkötés.Model;
+
+namespace virágkötés.WiewModel
+{
+    public class ViragViewModel:ViewModelBase
+    {
+        public Virágbolt v;
+
+        public RelayCommand Calculate {  get; set; }
+
+        public ViragViewModel()
+        {
+            v= new Virágbolt();
+            Calculate = new RelayCommand(execute => Calculation());
+        }
+
+        public string Tulipan
+        {
+            get { return v.Tulipandb.ToString(); }
+            set { v.Tulipandb = Convert.ToInt32(value);}
+        }
+
+        public string Rozsa
+        {
+            get { return v.Roszadb.ToString(); }
+            set { v.Roszadb = Convert.ToInt32(value); }
+        }
+
+        public string Narcisz
+        {
+            get { return v.Narciszdb.ToString(); }
+            set { v.Narciszdb = Convert.ToInt32(value); }
+        }
+
+        public string Osszeg
+        {
+            get 
+            { 
+                return v.Vegosszeg.ToString();
+            }
+        }
+
+        public void Calculation()
+        {
+            OnPropertyChanged("Osszeg");
+        }
+    }
+}
