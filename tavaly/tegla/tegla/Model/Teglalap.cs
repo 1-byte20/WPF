161a2f99cc6aa1533c1dc25065c0833640711f00
Bite Gergő Máté <153619640+1-byte20@users.noreply.github.com>
2024-11-06 15:10:16 +0100

    Add files via upload

---
+++ tavaly/tegla/tegla/Model/Teglalap.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tegla.Model
+{
+    public class Teglalap
+    {
+        public double oldal_1;
+        public double oldal_2;
+        public double terulet;
+        public double kerulet;
+        public double atlo;
+
+        public Teglalap()
+        {
+            oldal_1 = 1;
+            oldal_2 = 2;
+        }
+
+        public double Oldal1
+        {
+            get { return oldal_1; }
+            set { oldal_1 = value; }
+        }
+
+        public double Oldal2
+        {
+            get { return oldal_2; }
+            set { oldal_2 = value; }
+        }
+
+        public double Terulet
+        {
+            get { double ered= oldal_1*oldal_2; return ered; } 
+        }
+
+        public double Kerulet
+        {
+            get { double ered = 2 * (oldal_1 + oldal_2); return ered; }
+        }
+
+        public double Atlo
+        {
+            get { double ered = Math.Sqrt(Math.Pow(oldal_1,2) +Math.Pow(Oldal2,2)); return ered; }
+        }
+    }
+}
---
+++ tavaly/tegla/tegla/ViewModel/TeglalapViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using tegla.Model;
+using ViewModels.BaseClass;
+
+namespace tegla.ViewModel
+{
+    public class TeglalapViewModel:ViewModelBase
+    {
+        public Teglalap t;
+        public RelayCommand CalculateCommand { get; set; }
+        public RelayCommand CloseCommand { get; set; }
+
+        public TeglalapViewModel()
+        {
+            t = new Teglalap();
+            CalculateCommand = new RelayCommand(execute => Calculate());
+            CloseCommand = new RelayCommand(execute => Close());
+        }
+        public string Oldal_1
+        {
+            get { return t.Oldal1.ToString(); }
+            set { t.Oldal1 = Convert.ToDouble(value); }
+        }
+        public string Oldal_2
+        {
+            get { return t.Oldal2.ToString(); }
+            set { t.Oldal2 = Convert.ToDouble(value); }
+        }
+
+        public string Terulet
+        {
+            get { return t.Terulet.ToString(); }
+            
+        }
+
+        public string Kerulet
+        {
+            get { return t.Kerulet.ToString(); }
+
+        }
+
+        public string Atlo
+        {
+            get { return t.Atlo.ToString(); }
+        }
+
+        public void Calculate()
+        {
+            OnPropertyChanged("Terulet");
+            OnPropertyChanged("Kerulet");
+            OnPropertyChanged("Atlo");
+        }
+
+        public void Close()
+        {
+            Application.Current.MainWindow.Close();
+        }
+
+    }
+
+    
+}
