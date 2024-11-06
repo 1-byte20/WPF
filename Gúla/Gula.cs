eb505fdf199f328500c53525ca3fec0bf9321af2
Bite Gergő Máté <153619640+1-byte20@users.noreply.github.com>
2024-11-06 14:56:33 +0100

    Add files via upload

---
+++ Gúla/Gula.cs
@@ -0,0 +1,10 @@
+using System;
+using System.ComponentModel;
+
+namespace Gúla.Model
+{
+    public class Gula : INotifyPropertyChanged
+    {
+        
+    }
+}
---
+++ Gúla/Gúla/Model/Gula.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gúla.Model
+{
+    public class Gula
+    {
+        private double alapterulet;
+        private double palast;
+        private double magasv;
+
+        public Gula()
+        {
+            alapterulet = 0;
+            palast = 0;
+            magasv = 0;
+        }
+        public double Alapterulet
+        {
+            set
+            {
+                alapterulet = value;
+                OnPropertyChanged("Gúla");
+            }
+        }
+        public double Palast
+        {
+            set
+            {
+                palast = value;
+                OnPropertyChanged("Gúla");
+               
+            }
+        }
+
+        public double Magasv
+        {
+            set
+            {
+                magasv = value;
+                OnPropertyChanged("BetegAdatok");
+            
+            }
+        }
+
+        public double Felsz
+        {
+            get
+            {
+                double felsz = alapterulet + palast;
+                return felsz;
+            }
+        }
+
+        public double Terfog
+        {
+            get
+            {
+                double terfog = (alapterulet + magasv) / 3;
+                return terfog;
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
