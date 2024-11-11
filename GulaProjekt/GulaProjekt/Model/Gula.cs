bf9ee8583a5dd9513fd7c5930386c0b93100a0d6
Bite Gergő Máté <153619640+1-byte20@users.noreply.github.com>
2024-11-11 11:02:53 +0100

    Add files via upload

---
+++ GulaProjekt/GulaProjekt/Model/Gula.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GulaProjekt.Model
+{
+    public class Gula : INotifyPropertyChanged
+    {
+        public double alaplap;
+        public double palast;
+        public double magassag;
+        public double felszin;
+        public double terfogat;
+
+        public Gula()
+        {
+            alaplap = 1;
+            palast=1;
+            magassag = 1;
+        }
+
+        public double Alaplap
+        {
+            get { return alaplap;}
+            set { alaplap = value; }
+        }
+
+        public double Palast
+        {
+            get { return palast; }
+            set { palast = value; }
+        }
+
+        public double Magassag
+        {
+            get { return magassag; }
+            set { magassag = value; } 
+        }
+
+        public double Felszin
+        {
+            get { double equal = Alaplap + Palast; return equal; }
+
+        }
+
+        public double Terfogat
+        {
+            get { double equal = (Alaplap * Magassag) / 3; return equal; }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+    }
+
+
+}
